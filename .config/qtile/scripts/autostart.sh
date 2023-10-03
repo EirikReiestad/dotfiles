730f831355eb7d2739da40fff1b98291a110f304
EirikReiestad <eirik@reiestad.no>
2023-10-03 17:27:31 +0200

    qtile

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -3,9 +3,6 @@
 # Use "run program" to run it only if it is not already running
 # Use "program &" to run it regardless
 # ---
-# NOTE: This script runs with every restart of AwesomeWM
-# TODO: run_once
-
 
 function run {
     if ! pgrep $1 > /dev/null ;
